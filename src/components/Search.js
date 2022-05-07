0c07e319e38ac9f9d3e2d45cad545341ed6245e9
isijolatosin <isijolatosin2210@gmail.com>
2022-05-07 03:57:49 -0400

    page style implementing

--- src/components/Search.js
+++ src/components/Search.js
@@ -16,7 +16,7 @@ const Search = ({
 	}
 	return (
 		<form
-			className="w-[80%] lg:w-[50%] mt-10 flex items-center"
+			className="relative w-[80%] lg:w-[50%] mt-10 flex items-center"
 			onSubmit={onSearch}>
 			<div className="flex rounded-full items-center bg-blur2  h-[30px] w-[100%]">
 				<input
@@ -27,9 +27,18 @@ const Search = ({
 					placeholder="Search..."
 					className="focus:ring-0 w-full h-full text-sm bg-transparent border-0 text-white placeholder-white font-light"
 				/>
-				<span className="text-[10px] text-white font-light italic mr-3 border-l-[1px] pl-3">
-					joelarueyastudio
-				</span>
+				<div className="flex pr-3">
+					<span className="text-[10px] text-white font-light italic border-l-[1px] pl-3">
+						joelarueyastudio
+					</span>
+					{data.length > 0 && (
+						<span
+							onClick={Reset}
+							className="hover:cursor-pointer hover:text-cyan-900 text-[10px] text-yellow-600 font-light ml-3 ease duration-300 border-l-[1px] pl-3">
+							Reset
+						</span>
+					)}
+				</div>
 				{data?.length ? (
 					<div className=" rounded-r-full h-full flex flex-row items-center bg-yellow-600">
 						<RiSearch2Fill
@@ -46,13 +55,6 @@ const Search = ({
 					</div>
 				)}
 			</div>
-			{data.length > 0 && (
-				<span
-					onClick={Reset}
-					className="hover:cursor-pointer w-[100px] text-[10px] text-yellow-600 font-light ml-5">
-					Reset All
-				</span>
-			)}
 		</form>
 	)
 }
